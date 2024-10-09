a88d693aaf0b8dc591491ee80fd8069e1c9d344c
lavinejames <hello@jameslavine.com>
2024-10-09 13:21:56 -0400

    Add general.js

---
+++ general.js
@@ -0,0 +1,31 @@
+// // Custom context menu
+// const contextMenuTrigger = document.querySelector(".context-menu-trigger");
+// const contextMenu = document.querySelector(".context-menu");
+
+// contextMenuTrigger.addEventListener("contextmenu", function (e) {
+//   e.preventDefault();
+  
+//   // Show the context menu with a smooth opacity transition
+//   contextMenu.style.visibility = "visible";
+//   contextMenu.classList.add("cc-show");
+// });
+
+// document.body.addEventListener("click", function () {
+//   contextMenu.classList.remove("cc-show"); // Remove the class to hide the menu
+  
+//   // Wait for the opacity transition to finish (e.g., 300ms) before hiding the element
+//   setTimeout(function () {
+//     contextMenu.style.visibility = "hidden";
+//   }, 200); // Match the duration of the opacity transition
+// });
+
+// Change header background color on scroll 
+window.addEventListener('scroll', function() {
+  const root = document.documentElement;
+  
+  if (window.scrollY > 0) {
+    root.style.setProperty('--component-navbar--background-color', 'var(--colors-white--1000)');
+  } else {
+    root.style.setProperty('--component-navbar--background-color', 'var(--colors-white--300)');
+  }
+});
